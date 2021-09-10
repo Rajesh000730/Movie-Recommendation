1635685547f0edfd161673da86cd0766db334b1a
Rajesh000730 <rajeshrachit09@gmail.com>
2021-09-10 16:22:56 +0530

    second commit

--- myApp/main.py
+++ myApp/main.py
@@ -1,27 +1,49 @@
 
 from flask import Flask, render_template, request, redirect
-
+details= {"title" : " ",
+"hero" :  " ",
+"heroine" : " ",
+"date" :" ",
+"duration" : " ",
+"genre" : " ",
+ "imdb" : " ",
+"production":" ",
+"url":" ",
+"herourl" : " ",
+"heroineurl" : " "}
 app = Flask(__name__)
-title = " "
-hero =  " "
-heroine = " "
+
 @app.route("/")
 def home():
     return render_template('home.html')
 
 @app.route("/movie")
 def movie():
-    return render_template('recommendation.html', title=title, hero=hero, heroine=heroine)
+    if(details['title'] == " " or details['hero'] == " " or details['heroine'] == " "):
+        return render_template('error.html')
+    else:    
+        return render_template('recommendation.html', title=details['title'], hero=details['hero'], heroine=details['heroine'], date=details['date'], duration=details['duration'],
+        genre=details['genre'], imdb=details['imdb'], production=details['production'], url=details['url'], herourl=details['herourl'], heroineurl=details['heroineurl'])
 
 @app.route("/recommendation", methods=["POST"])
 def recommend():
-    global title    
-    global hero
-    global heroine
-    title = request.form['title'] 
-    hero = request.form['hero'] 
-    heroine = request.form['heroine'] 
-    return "success"      
+    global details
+    details['title'] = request.form['title'] 
+    details['hero'] = request.form['hero']
+    details['heroine'] = request.form['heroine']
+    details['date'] = request.form['date']
+    details['duration'] = request.form['duration']
+    details['genre'] = request.form['genre']
+    details['imdb'] = request.form['imdb']
+    details['production'] = request.form['production']
+    details['url'] = request.form['url']
+    details['herourl'] = request.form['herourl']
+    details['heroineurl'] = request.form['heroineurl']
+    if(details['title'] == " "):
+        return "failed"
+    else:
+        return "success"    
+
 if __name__ == '__main__':
     app.run(debug=True)  
 else:   
---
+++ myApp/static/js/home.js
@@ -0,0 +1,14 @@
+
+fetch("../static/data/data.json")
+.then(response => {
+   return response.json();
+})
+.then(data => {
+              
+                   
+
+});
+
+
+
+
